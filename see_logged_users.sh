9da8d65987e155e49b05a6bf3c2ea56be199cef1
Bogdan Doroslovac <bogdan@doroslovac.rs>
2020-05-02 23:28:39 +0200

    turining it into command

--- see_logged_users.sh
+++ see_logged_users.sh
@@ -1,35 +1,87 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]; then
-    printf "Too few arguments ($#), expected 1.\nUsage: $0 GROUP\n" >&2
+SHOW_LOGGED=0
+SHOW_NEVER_LOGGED=0
+
+function args()
+{
+    options=$(getopt -o lng: --long logged --long never-logged --long group: -- "$@")
+    [ $? -eq 0 ] || {
+        echo "Incorrect option provided"
+        exit 1
+    }
+    eval set -- "$options"
+    while true; do
+        case "$1" in
+        -l)
+            ;&
+        --logged)
+            SHOW_LOGGED=1
+            ;;
+        -n)
+            ;&
+        --never-logged)
+            SHOW_NEVER_LOGGED=1
+            ;;
+	-g)
+	    ;&
+	--group)
+	    shift;
+            GROUP=$1
+	    ;;
+	--)
+	    shift
+            break
+            ;;
+        esac
+        shift
+    done
+}
+ 
+args $0 "$@"
+
+if [ -z ${GROUP+x} ]; then
+    echo "Group is not set."
     exit 1
 fi
 
-GROUP=$1
-if ! grep -q $GROUP /etc/group; then
-    echo "group $GROUP does not exist"
+if [ -z $(getent group $GROUP) ]; then
+    echo "group $GROUP does not exist."
     exit 1
 fi
 
-USERS_SORTED=$(grep "$GROUP" /etc/group | sed 's/.*://;s/,/\n/g' | sort | uniq)
+if ! [ -z $(id -u $GROUP &> /dev/null) ]; then
+    echo "group $GROUP does not exist. It is a user name."
+    exit 1
+fi
+
+USERS_SORTED=$(getent group $GROUP | sed 's/.*://;s/,/\n/g' | sort | uniq)
 LOGGED_SORTED=$(last -w | awk '{print $1}' | sort | uniq | sed '/^$/d')
 
 LONGEST_USERNAME=$(awk '{print length}' <(echo "$USERS_SORTED") | sort -nr | head -1)
 
-#echo "Users in group $GROUP that have never logged in"
-#comm -23 <(echo "$USERS_SORTED") <(echo "$LOGGED_SORTED")
+for USER in $(comm -23 <(echo "$USERS_SORTED" ) <(echo "$LOGGED_SORTED")); do
+    NEV="${NEV}\n$(
+    awk '{
+    printf("| %'$LONGEST_USERNAME'-s | --- -- --:-- | ------- |",$1)
+    }' <(echo $USER))"
+done
+
+if [ "$SHOW_NEVER_LOGGED" -eq "1" ]; then
+    NEV="| USERNAME            | LAST LOGGED  | DURATION|$NEV"
+    echo -e "$NEV"
+fi
+
 
 for USER in $(comm -12 <(echo "$USERS_SORTED" ) <(echo "$LOGGED_SORTED")); do
-    TMP="${TMP}\n$(
+    LOG="${LOG}\n$(
     last -w "$USER" | head -n 1 |
     awk '{
     printf("| %'$LONGEST_USERNAME'-s | %3s %02d %s | %7-s |",$1, $5, $6, $7, $10)
     }')"
 done
 
-TMP="| USERNAME            | LAST LOGGED  | DURATION|
-$(sort -k4Mr,4 -k5nr,5 <(echo -e "$TMP"))"
-
-echo -e "$TMP"
-
-#print $1"\t\t"$(NF-5)" "$(NF-4)" "$(NF-3)" "$(NF-2)" "$(NF-1)" "$(NF)
+if [ "$SHOW_LOGGED" -eq "1" ]; then
+    LOG="| USERNAME            | LAST LOGGED  | DURATION|\n$(sort -k4Mr,4 -k5nr,5 <(echo -e "$LOG"))"
+    echo -e "$LOG"
+fi
