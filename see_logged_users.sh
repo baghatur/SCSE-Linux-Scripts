3b487f379c38230c99ba719cb813886da75ae900
Bogdan Doroslovac <bogdan@doroslovac.rs>
2020-05-02 20:41:30 +0200

    sort and show only logged users

--- see_logged_users.sh
+++ see_logged_users.sh
@@ -14,12 +14,22 @@ fi
 USERS_SORTED=$(grep "$GROUP" /etc/group | sed 's/.*://;s/,/\n/g' | sort | uniq)
 LOGGED_SORTED=$(last -w | awk '{print $1}' | sort | uniq | sed '/^$/d')
 
-echo "Users in group $GROUP that have never logged in"
-comm -23 <(echo "$USERS_SORTED") <(echo "$LOGGED_SORTED")
+LONGEST_USERNAME=$(awk '{print length}' <(echo "$USERS_SORTED") | sort -nr | head -1)
 
-echo -e "\n------\n"
+#echo "Users in group $GROUP that have never logged in"
+#comm -23 <(echo "$USERS_SORTED") <(echo "$LOGGED_SORTED")
 
-echo "Users in group $GROUP last logged in"
 for USER in $(comm -12 <(echo "$USERS_SORTED" ) <(echo "$LOGGED_SORTED")); do
-    last "$USER" | head -n 1 | awk '{print $1"\t\t"$(NF-5)" "$(NF-4)" "$(NF-3)" "$(NF-2)" "$(NF-1)" "$(NF)}'
+    TMP="${TMP}\n$(
+    last -w "$USER" | head -n 1 |
+    awk '{
+    printf("| %'$LONGEST_USERNAME'-s | %3s %02d %s | %7-s |",$1, $5, $6, $7, $10)
+    }')"
 done
+
+TMP="| USERNAME            | LAST LOGGED  | DURATION|
+$(sort -k4Mr,4 -k5nr,5 <(echo -e "$TMP"))"
+
+echo -e "$TMP"
+
+#print $1"\t\t"$(NF-5)" "$(NF-4)" "$(NF-3)" "$(NF-2)" "$(NF-1)" "$(NF)
